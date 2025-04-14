869eddfa727150231e62a7b450d8121420a29350
kanedaaaa <samflynn19121@gmail.com>
2025-04-14 07:44:28 +0400

    feat: signup handler

--- internal/app/handler/auth_handler.go
+++ internal/app/handler/auth_handler.go
@@ -1,1 +1,33 @@
 package handler
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/kanedaaaa/shortl/internal/app/service"
+)
+
+func SignupHandler(c *gin.Context) {
+	var userRequest struct {
+		Email    string `json:"email" binding:"required,email"`
+		Username string `json:"username" binding:"required"`
+		Password string `json:"password" binding:"required,min=8"`
+	}
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "invalid input data",
+		})
+		return
+	}
+
+	err := service.Signup(userRequest.Email, userRequest.Username, userRequest.Password)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"message": "ok",
+	})
+}
